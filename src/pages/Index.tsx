324e6045206081dc936ca698a3fd272f348ac4fc
Saurabh Sharma <osr.megha>
2020-11-17 23:29:47 +0530

    - working version

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,47 +1,14 @@
 import { Link } from "@react-navigation/native";
-import React, { useState } from "react";
+import React from "react";
 import { View } from "react-native";
 import { JsonForm } from "./components/json-form/JsonForm";
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const Index = (props) => {
-  const [formData, setFormData] = useState({
+  // form field values
+  const _formData = {
     username: "Saurabh",
     password: "Password@1",
-  });
-
-  const onBeforeSubmit = () => {
-    // allowed.current = true;
-  };
-
-  const onSuccess = (event) => {
-    console.log("onSuccess outer called");
-    const { response } = event.params;
-    console.log(response);
-    // setLoading(false);
-    // setMessage(response.data[controller][action].message);
-  };
-
-  const onError = (event) => {
-    console.log(event);
-    const { exceptions } = event.params;
-    const exceptionsMessages = exceptions.map((messages) =>
-      messages.join(", ")
-    );
-    // setLoading(false);
-    // if (exceptionsMessages.length) {
-    //   setException(exceptionsMessages.join("\n"));
-    // }
-  };
-
-  // const onErrorOk = () => setException(null);
-
-  // form data mutator
-  const onChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.params.name]: event.params.value,
-    });
   };
 
   // form schema
@@ -57,17 +24,25 @@ export const Index = (props) => {
     <View>
       <JsonForm
         schema={schema}
-        formData={formData}
-        onSuccess={(e) => {
-          onSuccess(e);
-          props.navigation.navigate("First");
-        }}
-        onError={onError}
-        onBeforeSubmit={onBeforeSubmit}
-        onChange={(e) => {
-          console.log("data changed");
-          onChange(e);
-        }}
+        _formData={_formData}
+        // _onBeforeSubmit={(e) => {
+        //   console.log("*** _onBeforeSubmit ***");
+        //   console.log(e);
+        // }}
+        // _onSubmit={(e) => {
+        //   console.log("*** _onSubmit ***");
+        //   console.log(e);
+        // }}
+        // _onError={(e) => {
+        //   console.log("*** _onError ***");
+        //   console.log(e);
+        // }}
+        // _onSuccess={(e) => {
+        //   props.navigation.navigate("First");
+        // }}
+        // _onChange={(e) => {
+        //   console.log("data changed");
+        // }}
       />
       <Link
         style={{
--- src/pages/components/json-form/JsonForm.tsx
+++ src/pages/components/json-form/JsonForm.tsx
@@ -1,26 +1,67 @@
 import { useNavigation } from "@react-navigation/native";
 import { createBrowserHistory } from "history";
-import React from "react";
+import React, { useState } from "react";
 import { Platform, StyleSheet } from "react-native";
 import Form from "react-native-web-jsonschema-form";
 import { MainContainer, UIProvider } from "react-native-web-ui-components";
 import useSafeSetState from "../../../utils/useSafeState";
 
 export const JsonForm = ({
-  onClose,
-  onBeforeSubmit,
-  onAfterSuccess,
+  _onBeforeSubmit,
+  _onSuccess,
+  _onError,
+  _onSubmit,
+  _onChange,
+  _formData,
   schema,
   uiSchema,
-  formData,
-  onSuccess,
-  onError,
-  onChange,
+  onClose,
   ...props
 }) => {
   const [loading, setLoading] = useSafeSetState(false);
   const [exception, setException] = useSafeSetState(null);
   const [message, setMessage] = useSafeSetState(null);
+  const [formData, setFormData] = useState(_formData);
+
+  const onBeforeSubmit = (e) => {
+    console.log("*** onBeforeSubmit ***");
+    console.log(e);
+    _onBeforeSubmit(e);
+  };
+
+  const onSuccess = (event) => {
+    console.log("onSuccess outer called");
+    const { response } = event.params;
+    console.log(response);
+    _onSuccess(event);
+    // setLoading(false);
+    // setMessage(response.data[controller][action].message);
+  };
+
+  const onError = (event) => {
+    console.log("*** onError ***");
+    console.log(event);
+    const { exceptions } = event.params;
+    const exceptionsMessages = exceptions.map((messages) =>
+      messages.join(", ")
+    );
+    _onError(event);
+    // setLoading(false);
+    // if (exceptionsMessages.length) {
+    //   setException(exceptionsMessages.join("\n"));
+    // }
+  };
+
+  const onErrorOk = () => setException(null);
+
+  // form data mutator
+  const onChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.params.name]: event.params.value,
+    });
+  };
+
   const theme = {
     input: {
       focused: StyleSheet.create({
@@ -35,6 +76,7 @@ export const JsonForm = ({
     await onBeforeSubmit(event);
     setLoading(true);
     const { values } = event.params;
+    _onSubmit(event);
     // const mutation = getMutation({
     //   values,
     //   controller,

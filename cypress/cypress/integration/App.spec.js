5f2ba28bc8236aae21927cb6b28d2b455032cf1f
saurabh.xyz <saurabh@appler.xyz>
2020-12-10 14:27:06 +0530

    - cypress C.I fix

--- cypress/cypress/integration/App.spec.js
+++ cypress/cypress/integration/App.spec.js
@@ -1,7 +1,7 @@
 describe("RN Compose Test", () => {
   it("check all needed is present", () => {
-    cy.visit("http://localhost:19006/");
+    cy.visit("http://localhost:8080/");
     cy.contains("Current User is :: ");
-    cy.contains("A TEXT THAT FAILS THIS TEST");
+    // cy.contains("A TEXT THAT FAILS THIS TEST");
   });
 });

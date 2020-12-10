d1ee76ed291b0b0a39d4c193742b6140a5e89ec4
saurshaz <saurabh@appler.xyz>
2020-12-10 13:13:12 +0530

    Update App.spec.js

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,5 +1,7 @@
 describe("RN Compose Test", () => {
   it("check all needed is present", () => {
     cy.visit("https://rn-compose.vercel.app/");
+    
+    cy.contains("A TEXT THAT FAILS THIS TEST");
   });
 });

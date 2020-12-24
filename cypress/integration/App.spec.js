ba19113767c2d3f845a438a8067f060f05fcbf3c
saurshaz <saurabh@appler.xyz>
2020-12-24 10:44:46 +0530

    Update App.spec.js

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,6 +1,9 @@
 describe("RN Compose Test", () => {
   it("check all needed is present", () => {
     cy.visit("http://localhost:8080/");
+    cy.get('[placeholder="Enter Keyword to Search"]')
+      .first()
+      .type(process.env.ADMIN_USER);
     console.log(`USER is ${process.env.ADMIN_USER}`);
     console.log(`PASSWORD is ${process.env.ADMIN_PWD}`);
     cy.contains("Current User is :: ");

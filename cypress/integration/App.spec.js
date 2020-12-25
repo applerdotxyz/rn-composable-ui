e7b812abd6242ba10c0f1b0cfc927ae473242cc0
saurabh.xyz <saurabh@appler.xyz>
2020-12-25 16:00:19 +0530

    - add cypress e2e test

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,13 +1,11 @@
 describe("RN Compose Test", () => {
   it("check all needed is present", () => {
-    cy.visit("http://localhost:8080/");
+    cy.visit(Cypress.env("DASHBOARD_URL"));
     expect(Cypress.env("ADMIN_PWD")).equal("Welcome@123");
-    cy.get('[placeholder="Enter Keyword to Search"]')
-      .first()
-      .type(Cypress.env("ADMIN_USER"));
+    cy.get("input").first().type(Cypress.env("ADMIN_USER"), { force: true });
     console.log(`USER is ${Cypress.env("ADMIN_USER")}`);
     console.log(`PASSWORD is ${Cypress.env("ADMIN_PWD")}`);
-    cy.contains("Current User is ::  ");
+    cy.contains("Search Filter Example");
     // cy.contains("A TEXT THAT FAILS THIS TEST");
   });
 });

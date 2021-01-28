388f3c282687df333040b8e001cb16d0dde739d5
saurabh.xyz <saurabh@appler.xyz>
2021-01-28 16:42:23 +0530

    - updated example config

--- examples/sagar-poc/layout.js
+++ examples/sagar-poc/layout.js
@@ -1,13 +1,11 @@
-import React from "react";
-import { Text } from "react-native";
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { About } from "../../src/components/About";
 import { ActionComp } from "../../src/components/ActionComp";
 import { Comp5 } from "../../src/components/Comp5";
-// import { JsonForm } from "./components/JsonForm";
 import { Home } from "../../src/components/Home";
 import { RandomPic } from "../../src/components/RandomPic";
 import { rowStyle, styles } from "../common";
-
+import { JsonForm } from "../../src/components/json-form/JsonForm";
 // All component which will be rendered
 export const componentsSet = {
   Comp5,
@@ -15,9 +13,8 @@ export const componentsSet = {
   Home,
   About,
   RandomPic,
-  // JsonForm
+  JsonForm,
 };
-
 // components section
 const schema = {
   type: "object",
@@ -26,52 +23,16 @@ const schema = {
     password: { type: "string" },
   },
 };
-
 export const routes = {};
-
 routes.routeOne = {
-  0: {
-    0: {
-      // layout: null // evict the earlier 1st column, other way is to hide it
+  "1.container": {
+    // col no
+    "1.1.leftNavCol": {
       layout: {
         colConfig: {
-          // colSize: 0, // *** change the colSize
-          colStyle: { display: "block" }, // *** hide the 1st column
+          colStyle: { display: "none" },
         },
       },
-    },
-    1: {
-      layout: {
-        1: {
-          // *** below we are adding a new row(this will replace current layout config values), and this will have 2 columns
-          0: {
-            // col no
-            colSize: 10,
-            idx: "About",
-            label: "comp5 >> changed at runtime 1",
-          },
-          1: {
-            // col no
-            colSize: 10,
-            idx: "About",
-            label: "comp5 >> changed at runtime 2",
-          },
-        },
-      },
-    },
-  },
-};
-
-routes.routeTwo = {
-  "1.container": {
-    "1.1.leftNavCol": {
-      layout: null, // evict the earlier 1st column, other way is to hide it
-      // layout: {
-      //   colConfig: {
-      //     // colSize: 0, // *** change the colSize
-      //     colStyle: { display: "none" }, // *** hide the 1st column
-      //   },
-      // },
     },
     "1.2.bodyCol": {
       layout: {
@@ -79,65 +40,251 @@ routes.routeTwo = {
           colSize: 11,
         },
         "1.2.1.bodyHeaderRow": {
+          rowConfig: {
+            rowSize: 2,
+            rowStyle: rowStyle,
+          },
           bodyHeader: {
-            idx: "ActionComp",
-          },
-        },
-        "1.2.2.bodyContentRow": {
-          bodyContent: {
-            idx: "RandomPic",
-          },
-        },
-        "1.2.3.bodyFooterRow": {
-          bodyFooter: {
+            colSize: 1,
             idx: "About",
+            label: "bodyHeader-changed at 1st",
           },
         },
       },
     },
   },
 };
-
+routes.routeTwo = {
+  // row no
+  "1.container": {
+    // col no
+    "1.1.leftNavCol": {
+      layout: {
+        colConfig: {
+          colStyle: { display: "none" },
+        },
+      },
+    },
+    "1.2.bodyCol": {
+      layout: {
+        colConfig: {
+          colSize: 11,
+        },
+        "1.2.1.bodyHeaderRow": {
+          rowConfig: {
+            rowSize: 2,
+            rowStyle: rowStyle,
+          },
+          bodyHeader: {
+            colSize: 1,
+            idx: "About",
+            label: "bodyHeader-changed at 2nd",
+            colStyle: {
+              borderColor: "cyan",
+              alignSelf: "none",
+              borderWidth: 4,
+              height: "50vh",
+              backgroundColor: "skyblue",
+            },
+          },
+          bodyHeader1: {
+            colSize: 1,
+            idx: "About",
+            label: "bodyHeader1",
+            colStyle: {
+              borderColor: "cyan",
+              alignSelf: "none",
+              borderWidth: 4,
+              height: "50vh",
+              backgroundColor: "red",
+            },
+          },
+        },
+        "1.2.2.bodyContentRow": {
+          bodyContent: {
+            colSize: 1,
+            idx: "Home",
+            label: "bodyContent",
+            colStyle: {
+              borderColor: "cyan",
+              alignSelf: "none",
+              borderWidth: 4,
+              height: "50vh",
+              backgroundColor: "yellow",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+routes.routeThree = {
+  // row no
+  "1.container": {
+    // col no
+    "1.1.leftNavCol": {
+      layout: {
+        colConfig: {
+          colStyle: { display: "none" },
+        },
+      },
+    },
+    "1.2.bodyCol": {
+      layout: {
+        colConfig: {
+          colSize: 11,
+        },
+        "1.2.1.bodyHeaderRow": {
+          rowConfig: {
+            rowSize: 1,
+            rowStyle: rowStyle,
+          },
+          bodyHeader: {
+            
+              colStyle: { display: "none" },
+            
+          },
+          bodyHeader1: {
+            colSize: 11,
+            idx: "About",
+            label: "bodyHeader1-changed 1st",
+            colStyle: {
+              height: "100vh",
+            },
+          },
+        },
+        "1.2.2.bodyContentRow": {
+          bodyContent: {
+            colStyle: {
+              display: "none",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+// *************************************************
+//  Layout config
+// *************************************************
+// links row
+const links = {
+  "/": {
+    style: styles.navItem,
+    linkStyle: styles.tabName,
+    linkText: "Home",
+  },
+  "/about": {
+    style: styles.navItem,
+    linkStyle: styles.tabName,
+    linkText: "Feed",
+  },
+  "/contact": {
+    style: styles.navItem,
+    linkStyle: styles.tabName,
+    linkText: "Messages",
+  },
+};
+export const appConfig = {
+  /// 1st layout
+  componentsSet,
+  links, // FIXME: links mess up the styling in dynamic page transitions. pls look at the fix
+  layout: {
+    // row no
+    "1.container": {
+      rowConfig: {
+        rowSize: 1,
+        rowStyle: rowStyle,
+      },
+      // col no
+      "1.1.leftNavCol": {
+        layout: {
+          colConfig: {
+            colSize: 3,
+            colStyle: { backgroundColor: "grey" },
+          },
+          "1.1.leftNavHeaderRow": {
+            // row no
+            rowConfig: {
+              rowSize: 1,
+              rowStyle: rowStyle,
+            },
+            leftNavHeader: {
+              // col no
+              colSize: 1,
+              idx: "Home",
+              label: "leftNavHeader",
+              colStyle: {
+                borderColor: "cyan",
+                borderWidth: 4,
+                height: "100vh",
+                backgroundColor: "lightgreen",
+              },
+            },
+          },
+        },
+      },
+      "1.2.bodyCol": {
+        rowConfig: {
+          rowSize: 1,
+          rowStyle: rowStyle,
+        },
+        layout: {
+          colConfig: {
+            colSize: 11,
+            colStyle: {
+              backgroundColor: "grey",
+            },
+          },
+          "1.2.1.bodyHeaderRow": {
+            rowConfig: {
+              rowSize: 1,
+              rowStyle: rowStyle,
+            },
+            bodyHeader: {
+              // col no
+              colSize: 1,
+              idx: "About",
+              label: "bodyHeader",
+              colStyle: {
+                borderColor: "cyan",
+                alignSelf: "center",
+                borderWidth: 4,
+                height: "80vh",
+                backgroundColor: "skyblue",
+              },
+            //   props:{schema:schema}
+            },
+          },
+        },
+      },
+    },
+  },
+};
 // *************************************************
 //  "../applications/app-one/screen-one";
 // *************************************************
 // bind events to
 //  logic that binds
-
 export const events = {
-  /// <label>
   //<label>-<element-id> : <handler>
   "leftNavHeader-btn-one": {
     // <event> :: <handler>
-    onPress: (setLayoutConfig, setAppState) => {
-      // setLayoutConfig(routes["routeTwo"]);
-      setAppState({
-        bodyFooter: {
-          ui: "ActionComp",
-          props: { label: "bodyFooter" },
-          children: <Text>Hello from RandomPic</Text>,
-        },
-        bodyContent: {
-          ui: "RandomPic",
-          props: { label: "actioncomp-2" },
-          children: null,
-        },
-      });
-    },
-  },
-  //<label>-<element-id>
-  "leftNavHeader-btn-two": {
-    // <event> :: <handler>
-    onPress: (setLayoutConfig, setAppState) => {
+    onPress: (setLayoutConfig) => {
       setLayoutConfig(routes["routeOne"]);
     },
   },
-
-  // <label>
-  "leftNavBody-btn-two": {},
-  "leftNavBody-btn-one": {},
+  "bodyHeader-changed at 1st-btn-one": {
+    onPress: (setLayoutConfig) => {
+      setLayoutConfig(routes["routeTwo"]);
+    },
+  },
+  "bodyHeader1-btn-one": {
+    onPress: (setLayoutConfig) => {
+      setLayoutConfig(routes["routeThree"]);
+    },
+  },
 };
-
 // *************************************************
 //  Helper Util
 // *************************************************
@@ -146,165 +293,10 @@ export const getEvents = (elId, setLayoutConfig, setAppState) => {
   const elEvents = {};
   events[elId] &&
     Object.keys(events[elId]).map((eventName) => {
-      // console.log({ [eventName]: events[elId][eventName] });
       elEvents[eventName] = () =>
         events[elId] && events[elId][eventName] && events[elId][eventName]
           ? events[elId][eventName](setLayoutConfig, setAppState)
           : {};
     });
-  // console.log(elEvents);
   return elEvents;
-};
-
-// *************************************************
-//  Layout config
-// *************************************************
-
-export const appConfig = {
-  /// 1st layout
-  componentsSet,
-  links: {
-    "/": {
-      style: styles.navItem,
-      linkStyle: styles.tabName,
-      linkText: "Home",
-    },
-    "/about": {
-      style: styles.navItem,
-      linkStyle: styles.tabName,
-      linkText: "Feed",
-    },
-  },
-  layout: {
-    colConfig: {
-      colSize: 1,
-    },
-    // row no
-    "1.container": {
-      rowConfig: {
-        rowSize: 1,
-        style: rowStyle,
-      },
-      // col no
-      "1.1.leftNavCol": {
-        layout: {
-          colConfig: {
-            colSize: 3,
-          },
-          "1.1.leftNavHeaderRow": {
-            // row no
-            rowConfig: {
-              // rowSize: 1,
-              rowStyle: {
-                height: "10vh",
-              },
-            },
-            leftNavHeader: {
-              // col no
-              colSize: 1,
-              idx: "Home",
-              label: "home",
-              colStyle: { borderWidth: 1 },
-            },
-          },
-          "1.1.leftNavBodyRow": {
-            // row no
-            rowConfig: {
-              // rowSize: 1,
-              rowStyle: {
-                height: "90vh",
-              },
-            },
-            leftNavBody: {
-              // col no
-              colSize: 1,
-              idx: "Home",
-              label: "leftNavBody",
-              colStyle: { borderWidth: 1 },
-            },
-          },
-        },
-      },
-      "1.2.bodyCol": {
-        layout: {
-          colConfig: {
-            colSize: 11,
-          },
-          "1.2.1.bodyHeaderRow": {
-            // row no
-            rowConfig: {
-              rowStyle: { height: "40vh" },
-            },
-            bodyHeader: {
-              // col no
-              colSize: 2,
-              idx: "RandomPic",
-              label: "bodyHeader",
-              schema,
-              colStyle: { borderWidth: 1 },
-            },
-            bodyHeader1: {
-              // col no
-              colSize: 9,
-              idx: "Home",
-              label: "bodyHeader1",
-              schema,
-              colStyle: { borderWidth: 2, height: "8vh" },
-            },
-          },
-          //   bodyHeader2: {
-          //     // col no
-          //     colSize: 9,
-          //     idx: "Home",
-          //     label: "bodyHeader2",
-          //     schema,
-          //     colStyle: { borderWidth: 2, height: "20vh" },
-          //   },
-          //   "1.2.2.bodyContentCol": {
-          //     layout: {
-          //       colConfig: {
-          //         colSize: 11,
-          //       },
-          "1.2.2.bodyContentRow": {
-            // row no
-            rowConfig: {
-              rowStyle: { height: "60vh" },
-            },
-            bodyContent: {
-              // col no
-              colSize: 2,
-              idx: "Home",
-              label: "bodyContent",
-              schema,
-              colStyle: { borderWidth: 1 },
-            },
-            bodyContent1: {
-              // col no
-              colSize: 9,
-              idx: "About",
-              label: "aboutComponent",
-              schema,
-              colStyle: { borderWidth: 1 },
-            },
-          },
-          //     },
-          //   },
-        },
-      },
-    },
-    "2.container": {
-      // row no
-      rowConfig: {
-        rowSize: "0.21",
-        style: rowStyle,
-      },
-      footer: {
-        // col no
-        colSize: 1,
-        idx: "ActionComp",
-        label: "footer",
-        colStyle: { borderWidth: 4 },
-      },
-    },
-  },
 };

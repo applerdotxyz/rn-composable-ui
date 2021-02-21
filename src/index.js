f4817e5ed15df3ef847c3fbe78483d7da4a88144
saurabh.xyz <saurabh@appler.xyz>
2021-02-21 19:27:12 +0530

    - move WrappedApp out - take all logic needed into library

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-
 // ****** EXAMPLE CONFIGS START ****************
+// FIXME: fix the component mount stage example with <label>-$init logic fix
 
-import {
-  appConfig,
-  routes,
-  getEvents,
-  getInitEvents,
-} from "./rn-config-tyler/packages/demo/examples/sagar-poc/component-mount/layout";
+// import {
+//   appConfig,
+//   routes,
+//   getEvents,
+//   getInitEvents,
+// } from "./rn-config-tyler/packages/demo/examples/sagar-poc/component-mount/layout";
 
 // import { appConfig, routes, getEvents } from "./rn-config-tyler/packages/demo/examples/todo-app/layout";
 // import { appConfig, routes, getEvents } from "./rn-config-tyler/packages/demo/examples/sagar-poc/example1";
@@ -31,12 +31,12 @@ import {
 //   getEvents,
 // } from "./rn-config-tyler/packages/demo/examples/sagar-poc/with-setLayout/layout";
 
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-//   getInitEvents,
-// } from "./rn-config-tyler/packages/demo/examples/sagar-poc/3_4-screen-example-web/layout";
+import {
+  appConfig,
+  routes,
+  getEvents,
+  getInitEvents,
+} from "./rn-config-tyler/packages/demo/examples/sagar-poc/3_4-screen-example-web/layout";
 
 // import {
 //   appConfig,
@@ -74,8 +74,14 @@ import {
 //   getEvents,
 // } from "./rn-config-tyler/packages/demo/examples/another-grid/layout"; /// another example with changes
 
+// import {
+//   appConfig,
+//   getEvents,
+//   getInitEvents,
+//   routes,
+// } from "./rn-config-tyler/packages/demo/examples/sagar-poc/with-appstate/layout";
 // ****** EXAMPLE CONFIGS END ****************
-import WrappedApp from "./WrappedApp";
+import { App } from "./rn-config-tyler/packages/demo/helpers/lib/src";
 
 // **************************************************
 // TODO uncomment below, and comment section at very bottom for non-codesandbox
@@ -84,8 +90,8 @@ import WrappedApp from "./WrappedApp";
 const { registerRootComponent } = require("expo");
 registerRootComponent(() => (
   // {/* FIXME: debug=true below results in error */}
-  <WrappedApp
-    appConfig={appConfig}
+  <App
+    config={appConfig}
     routes={routes}
     debug={false}
     getEvents={getEvents}

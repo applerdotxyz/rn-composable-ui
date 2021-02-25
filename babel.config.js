c7311358446f594a5d07ff018b4ed695ce1de6f3
saurabh.xyz <saurabh@appler.xyz>
2021-02-25 20:28:26 +0530

    - customize expo webpack

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          root: ["./src"],
-          extensions: [".js", ".json", ".ts", "tsx"],
-          // alias: {
-          //   src: "./src",
-          //   components: "./src/rn-config-tyler/packages/demo/components",
-          //   examples: "./src/rn-config-tyler/packages/demo/examples",
-          //   "rn-config-tyler":
-          //     "./src/rn-config-tyler/packages/demo/helpers/lib",
-          // },
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo'],
   };
 };

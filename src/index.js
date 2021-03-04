face2476a8a78b7ddd45515f12f3247bc4797638
saurshaz <saurabh@appler.xyz>
2021-03-04 11:31:29 +0530

    Remote config (#53)
    * - customize expo webpack
    
    * - added tailwind support
    
    * update the connected submodule
    
    * change the way we define layout config in code
    
    * minor adjustments for remote-config, functioning code
    
    * functioning local config (from config switch and remote call data
    )
    
    * remote config wip
    
    * refactoring
    
    * update submodule

--- src/index.js
+++ src/index.js
@@ -1,105 +1,39 @@
-import React from "react";
 // ****** EXAMPLE CONFIGS START ****************
 // FIXME: fix the component mount stage example with <label>-$init logic fix
+/*** example with json based forms  */
+const configs = {
+  "with-charts": "with-charts",
+  "with-jsonforms": "with-jsonforms",
+  "another-grid": "another-grid",
+  "3_4-screen-example-mobile": "3_4-screen-example-mobile",
+  "3_4-screen-example-web": "3_4-screen-example-web",
+  "with-calendar": "with-calendar",
+  "component-mount": "component-mount",
+  "todo-app": "todo-app",
+  "with-appstate": "with-appstate",
+  "with-setLayout": "with-setLayout",
+  "with-setLayout (without hide)": "with-setLayout (without hide)",
+  "collapsible-leftnav": "collapsible-leftnav",
+};
+const selected = "with-jsonforms";
 
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-//   // getInitEvents,
-// } from "./rn-config-tyler/packages/demo/examples/component-mount/layout";
+let moduleConfig = require(`./rn-config-tyler/packages/demo/examples/${configs[selected]}/layout`);
+const getComponents = moduleConfig.getComponents;
+const fetchConfig = moduleConfig.fetchConfig;
 
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/todo-app/layout";
+// ****** EXAMPLE CONFIGS END ****.************
+import React from "react";
+import { registerRootComponent } from "expo";
+import { App, init } from "./rn-config-tyler/packages/demo/helpers/lib/src";
 
-
-// import {
-//   appConfig,
-//   getEvents,
-//   routes,
-// } from "./rn-config-tyler/packages/demo/examples/with-appstate/layout";
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/with-setLayout/layout";
-
-import {
-  appConfig,
-  routes,
-  getEvents,
-} from "./rn-config-tyler/packages/demo/examples/with-setLayout (without hide)/layout";
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-//   //   getInitEvents,
-// } from "./rn-config-tyler/packages/demo/examples/3_4-screen-example-web/layout";
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/with-calendar/layout";
-
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/with-charts/layout";
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/3_4-screen-example-mobile/layout";
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/with-jsonforms/layout";
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/collapsible-leftnav/layout"; /// example with button clicks and routing with dynamic changes to screen
-
-// import {
-//   appConfig,
-//   routes,
-//   getEvents,
-// } from "./rn-config-tyler/packages/demo/examples/another-grid/layout"; /// another example with changes
-
-// ****** EXAMPLE CONFIGS END ****************
-import { App } from "./rn-config-tyler/packages/demo/helpers/lib/src";
-
-// **************************************************
-// TODO uncomment below, and comment section at very bottom for non-codesandbox
-// **************************************************
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { registerRootComponent } = require("expo");
-registerRootComponent(() => (
-  // {/* FIXME: debug=true below results in error */}
-  <App
-    config={appConfig}
-    routes={routes}
-    debug={false}
-    getEvents={getEvents}
-    // getInitEvents={getInitEvents}
-  />
-));
+init(moduleConfig, fetchConfig, getComponents).then((passProps) => {
+  registerRootComponent(() => <App debug={true} {...passProps} />);
+});
 
-// **************************************************
+// **************************************************/
 // TODO: below section to make it run on codesandbox.io
-// **************************************************
+/// **************************************************
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 // const { render } = require("react-dom");

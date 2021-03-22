d299f7fec3c297c0964928152d8cc46d51255243
sagar0211 <sagarprihar002@gmail.com>
2021-03-22 20:39:54 +0530

    main.yml modified and test added

--- cypress/integration/examples/with-appState.test.js
+++ cypress/integration/examples/with-appState.test.js
@@ -1,28 +1,35 @@
 describe("with-appState", () => {
-    it("Config test", () => {
-      // cy.visit("http://localhost:19006/");
-      cy.visit("http://localhost:8080/");
+  it("Config test", () => {
+    // cy.visit("http://localhost:19006/");
+    cy.visit("http://localhost:8080/");
 
-      cy.contains("Examples");
-      cy.get("select").select("with-appstate");
-      cy.contains("Home *** leftNavHeader");
-      cy.contains("Home *** leftNavBody");
-      cy.contains("`RandomPic *** bodyHeader`");
-      cy.contains("Home *** bodyHeader1");
-      cy.contains("Home *** bodyContent");
-      cy.contains("Home *** bodyContent1");
-      cy.contains("Home *** bodyContent2");
-      cy.contains("footer");
-      // cy.matchImageSnapshot('initialLayout');
-      cy.screenshot();
-      cy.get(`[data-testid="leftNavHeader-btn-one"]`).contains("ACT1").click();
-      // cy.matchImageSnapshot('layoutChanged');
-      cy.contains("About *** bodyHeader");
-      cy.contains("`RandomPic *** bodyContent`");
-      cy.screenshot();
-
-
-
+    cy.contains("Examples");
+    cy.get("select").select("with-appstate");
+    cy.contains("Home *** leftNavHeader");
+    cy.contains("Home *** leftNavBody");
+    cy.contains("`RandomPic *** bodyHeader`");
+    cy.contains("Home *** bodyHeader1");
+    cy.contains("Home *** bodyContent");
+    cy.contains("Home *** bodyContent1");
+    cy.contains("Home *** bodyContent2");
+    cy.contains("footer");
+    // cy.matchImageSnapshot('initialLayout');
+    // cy.screenshot();
+    cy.document().toMatchImageSnapshot({
+      "name": "Before route",            // Naming resulting image file with a custom name rather than concatenating test titles
+      "separator": "initial",            // Naming resulting image file with a custom separator rather than using the default ` #`
+    })
+    cy.get(`[data-testid="leftNavHeader-btn-one"]`).contains("ACT1").click();
+    cy.document().toMatchImageSnapshot({
+      "name": "After route",            // Naming resulting image file with a custom name rather than concatenating test titles
+      "separator": "final",            // Naming resulting image file with a custom separator rather than using the default ` #`
     });
+    // cy.matchImageSnapshot('layoutChanged');
+    cy.contains("About *** bodyHeader");
+    cy.contains("`RandomPic *** bodyContent`");
+    // cy.screenshot();
+
+
+
   });
-  
+});
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,12 @@
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+// const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+
+// module.exports = (on, config) => {
+//   addMatchImageSnapshotPlugin(on, config);
+// };
+
+const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 
 module.exports = (on, config) => {
-  addMatchImageSnapshotPlugin(on, config);
+  initPlugin(on, config);
+  return config;
 };

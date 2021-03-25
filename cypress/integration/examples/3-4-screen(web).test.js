2ac1d07eda84431726021eb75b0baa2cc2572358
sagar0211 <44940025+sagar0211@users.noreply.github.com>
2021-03-25 17:46:56 +0530

    Config tests added (#66)
    * initial
    
    * initial
    
    * example added
    
    * example layout modification
    
    * examples added with appstate and setLayout
    
    * example added
    
    * JsonForm added in example
    
    * examples in one folder & 4 screen poc added
    
    * examples added with latest dev pull
    
    * Update App.tsx
    
    * Update About.tsx
    
    * Update About.tsx
    
    * JsonForm update modification
    
    * Calendar Component added and few layout modification
    
    * layout modification and calendar fix
    
    * Update layout.js
    
    * app.tsx and JS.tsx removed and index updated
    
    * setLayout example fix
    
    * mnor change in layout and listing
    
    * Agenda view added
    
    * charts added
    
    * submodules setup
    
    * submodules setup successfully
    
    * example folder removed and existing examples added in rn-config
    
    * example structure modification and more charts added
    
    * dropdown feature initial implementaton
    
    * dropdown initial
    
    * dropdown initial
    
    * tailwind example added
    
    * initial
    
    * setup
    
    * Tailwind layout and demo modification
    
    * cypress docker files added
    
    * cypress test added (setLayout wwithout hide)
    
    * 3-4 screen web,tailwindform tests added
    
    * layouts adjustments and tests added for layouts
    
    * test modified of 3-4 screen(web)
    
    * tests corrected
    
    * build tested and package.json modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * package.json modified
    
    * package.json modified
    
    * main.yml modified
    
    * package.json and main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified and test added
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * main.yml modified
    
    * Update main.yml
    
    * tests added
    
    * main.yml updated
    
    * main.yml and tests  updated
    
    * main.yml updated
    
    * main.yml updated
    
    * Readme updated
    
    * Tests updated with baseUrl
    
    * layout modified
    
    * package.json updated
    
    Co-authored-by: saurshaz <saurabh@appler.xyz>
    Co-authored-by: sagar0211 <sagarprihar002@gmail.com>

---
+++ cypress/integration/examples/3-4-screen(web).test.js
@@ -0,0 +1,31 @@
+describe("3-4-screen(web)", () => {
+    it("Config test", () => {
+      cy.visit("/");
+
+      cy.contains("Examples");
+      cy.get("select").select("3_4-screen-example-web");
+      cy.contains("Category").click();
+      cy.contains("Sub Category");
+      cy.contains("bodyHeader");
+      cy.contains("Home *** bodyContent");
+      cy.get(`input[value="8654787549"]`).clear().type("9874563210").blur();
+      cy.get(`input[value="654789"]`).clear().type("12345");
+      cy.contains("Submit").click();
+      cy.contains("Submit").click();
+      
+      //below one is givin problem related to re-ender tried pause aand also wait
+      // cy.get(`[data-testid="phone_num_verify"]`).wait(5000).contains("Verified Phone No:- 9874563210");
+      // cy.get(`[data-testid="phone_num_verify"]`).pause().contains("Verified Phone No:- 9874563210");
+      // cy.get(`[data-testid="phone_num_verify"]`).contains("Verified Phone No:- 9874563210");
+      cy.contains("Verified Phone No:- 9874563210");
+      cy.contains("Submit").click();
+      cy.contains("Name");
+      cy.contains("Category");
+      cy.contains("SubCategory");
+      cy.get(`input[placeholder="Enter Keyword to Search"]`).type("shah");
+      cy.contains("Mr. Shah");
+      cy.contains("Electricity");
+      cy.contains("Short circuit");
+    });
+  });
+  
---
+++ cypress/integration/examples/dynamic-navigation.test.js
@@ -0,0 +1,19 @@
+describe("dynamic-navigation", () => {
+    it("Config test", () => {
+      cy.visit("/");
+      cy.contains("Examples");
+      cy.get("select").select("dynamic-navigation");
+      cy.contains("Home *** leftNavHeader");
+      cy.contains("About *** bodyHeader");
+      cy.contains("ACT1").click();
+      cy.contains("About *** bodyHeader-changed at 1st");
+      cy.contains("AboutFlash").click();
+      cy.contains("About *** bodyHeader-changed at 2nd");
+      cy.contains("About *** bodyHeader1");
+      cy.contains("Home *** bodyContent");
+      cy.get(`[data-testid="bodyHeader1-btn-one"]`).contains("AboutFlash").click();
+      cy.contains("About *** bodyHeader1-changed 1st");
+      cy.contains("AboutFlash");
+    });
+  });
+  

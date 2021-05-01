ee4fcfaf826610fd3135ee1c0f70556091290bd3
saurabh.xyz <saurabh@appler.xyz>
2021-05-01 11:52:55 +0530

    merge latest from develop in tw-grid

--- src/index.js
+++ src/index.js
@@ -2,34 +2,40 @@
 // FIXME: fix the component mount stage example with <label>-$init logic fix
 /*** example with json based forms  */
 const configs = {
-  "3_4-screen-example-mobile": "3_4-screen-example-mobile",
-  "3_4-screen-example-web": "3_4-screen-example-web",
-  "another-grid": "another-grid",
-  "app-one": "app-one",
-  "app-three": "app-three",
-  "app-two": "app-two",
-  "collapsible-leftnav": "collapsible-leftnav",
-  "component-mount": "component-mount",
-  "dashboard-demo": "dashboard-demo",
-  "dynamic-navigation": "dynamic-navigation",
-  "react-router-port": "react-router-port",
-  "state-mgmt": "state-mgmt",
-  "todo-app": "todo-app",
-  "vanilla-grid-layout": "vanilla-grid-layout",
-  "with-appstate": "with-appstate",
-  "with-calendar": "with-calendar",
-  "with-charts": "with-charts",
-  "with-jsonforms": "with-jsonforms",
-  "with-setLayout": "with-setLayout",
-  "with-setLayout (without hide)": "with-setLayout (without hide)",
-  "with-tailwind": "with-tailwind",
-  "tw-grid-layout": "tw-grid-layout",
+  "3_4-screen-example-web": {
+    id: "3_4-screen-example-web",
+    desc: "demo for router with navigation for web",
+  },
+  "3_4-screen-example-mobile": {
+    id: "3_4-screen-example-mobile",
+    desc: "demo for router with navigation for mobile",
+  },
+  "another-grid": { id: "another-grid", desc: "demo for grid" },
+  "tw-grid": { id: "tw-grid-layout", desc: "demo for grid with tailwind" },
+  "app-one": { id: "app-one" },
+  "app-three": { id: "app-three" },
+  "app-two": { id: "app-two" },
+  "collapsible-leftnav": { id: "collapsible-leftnav" },
+  "component-mount": { id: "component-mount" },
+  "dashboard-demo": { id: "dashboard-demo" },
+  "dynamic-navigation": { id: "dynamic-navigation" },
+  "react-router-port": { id: "react-router-port" },
+  "todo-app": { id: "todo-app" },
+  "vanilla-grid-layout": { id: "vanilla-grid-layout" },
+  "with-appstate": { id: "with-appstate" },
+  "with-calendar": { id: "with-calendar" },
+  "with-charts": { id: "with-charts" },
+  "with-jsonforms": { id: "with-jsonforms" },
+  "with-setLayout": { id: "with-setLayout" },
+  "with-setLayout (without hide)": { id: "with-setLayout (without hide)" },
+  "with-tailwind": { id: "with-tailwind" },
 };
 // FIXME: LOAD ABOVE OBJECT dynamically
-const selected = "tw-grid-layout";
+// const selected = "tw-grid-layout";
 
+const selected = "3_4-screen-example-web";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-let moduleConfig = require(`./rn-config-tyler/packages/demo/examples/${configs[selected]}/layout`);
+let moduleConfig = require(`./rn-config-tyler/packages/demo/examples/${configs[selected]?.id}/layout`);
 const getComponents = moduleConfig.getComponents;
 const fetchConfig = moduleConfig.fetchConfig;
 

dca9d27bfae51a8a54f8a9e54e733223d0f9ba95
Nihal Kumar <nihal07kumar@hotmail.com>
2021-06-10 11:52:40 +0530

    New Layout

--- config/endpoint.ts
+++ config/endpoint.ts
@@ -1,3 +1,3 @@
-// export const SERVER_ENDPOINT = "http://localhost:8080/transaction-web/";
-export const SERVER_ENDPOINT =
-  "http://ec2-13-232-195-159.ap-south-1.compute.amazonaws.com:8080/transaction-web/";
+export const SERVER_ENDPOINT = "http://localhost:8080/transaction-web/";
+// export const SERVER_ENDPOINT =
+//   "http://ec2-13-232-195-159.ap-south-1.compute.amazonaws.com:8080/transaction-web/";
